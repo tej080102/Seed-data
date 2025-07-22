4fea3ea5f110452e05838f6f159c1bb86920dbcb
tej080102 <77581847+tej080102@users.noreply.github.com>
2025-07-22 18:04:11 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -59,7 +59,7 @@ uploaded_file = st.file_uploader("Upload Image", type=["jpg", "jpeg", "png"])
 
 if uploaded_file:
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
 
     if st.button("🔍 Classify Seed Type"):
         with st.spinner("Analyzing..."):
